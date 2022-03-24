e68644a2f7c98ed32791045ae6be5763e7ce7603
NguyenHungCuong2203 <92001656+kero223@users.noreply.github.com>
2022-03-24 10:08:11 +0700

    Update Form1.Designer.cs

--- Demo/Demo/Form1.Designer.cs
+++ Demo/Demo/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bt_thoat
@@ -89,12 +90,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 212);
+            this.button1.Location = new System.Drawing.Point(59, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 14;
             this.button1.Text = "Đăng Nhập";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Đăng Nhập";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -102,6 +112,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(314, 271);
             this.Controls.Add(this.bt_thoat);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.bt_dn);
             this.Controls.Add(this.txt_mk);
@@ -124,6 +135,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
